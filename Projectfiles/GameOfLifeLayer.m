23e4f256ffae227358f911accb2dc47658abd970
Daniel Garcia <danielgarcia2@gmail.com>
2014-02-16 21:16:48 -0500

    Added the update method with ccTime

--- Projectfiles/GameOfLifeLayer.m
+++ Projectfiles/GameOfLifeLayer.m
@@ -55,13 +55,11 @@ float priorY = 500;
             [numNeighbors addObject:subArr];
         }
 	}
-
 	return self;
 }
 
 -(void) draw
 {
-    
     //this draws a rectangle
     ccColor4F rectColor = ccc4f(0.5, 0.5, 0.5, 1.0); //red, green, blue, and alpha/transparancy
     ccDrawSolidRect(ccp(0,0 + Y_OFF_SET), ccp(WIDTH_GAME, HEIGHT_GAME + Y_OFF_SET), rectColor);
@@ -102,5 +100,110 @@ float priorY = 500;
     ccDrawSolidRect(ccp(0, HEIGHT_GAME + Y_OFF_SET), ccp(WIDTH_WINDOW / 2, HEIGHT_WINDOW + Y_OFF_SET), clearButtonColor);
     
 }
+
+
+
+-(void) update: (ccTime) delta
+{
+    KKInput* input = [KKInput sharedInput];
+    CGPoint pos = [input locationOfAnyTouchInPhase:KKTouchPhaseAny];
+    
+    if (input.anyTouchBeganThisFrame)
+    {
+        //get x and y coordinates for mouse
+        int x = pos.x;
+        int y = pos.y;
+        if(x >= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+        {
+            if(done == true)
+            {
+                done = false;
+            }
+            else
+            {
+                done = true;
+            }
+        }
+        else if(x <= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+        {
+            [self resetArrays];
+            done = true;
+        }
+        else
+        {
+            int row = (y - Y_OFF_SET) / CELL_WIDTH;
+            int col = x / CELL_WIDTH;
+            
+            //make corresponding position in array alive or dead
+            if([[[grid objectAtIndex:row] objectAtIndex: col] integerValue] == 1)
+            {
+                [[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@0];
+                //an abbreviation for [NSNumber numberWithInt: 0] is @0
+            }
+            else
+            {
+                [[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@1];
+                //an abbreviation for [NSNumber numberWithInt: 1] is @1
+            }
+        }
+    }
+    else if (input.anyTouchEndedThisFrame)
+    {
+        priorX = 500;
+        priorY = 500;
+    }
+    else if(input.touchesAvailable) //when the mouse is dragged while clicked the cells toggle on and off
+    {
+		{
+			//get x and y coordinates for mouse
+			float x = pos.x;
+			float y = pos.y;
+			
+			if(x >= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+			{
+			}
+			else if(x <= WIDTH_WINDOW / 2 && y > HEIGHT_GAME + Y_OFF_SET)
+			{
+			}
+			else
+			{
+				int row = (int)(y - Y_OFF_SET) / CELL_WIDTH;
+				int col = (int)x / CELL_WIDTH;
+				float y1 = (float) ((y - 21.0) / 20.0);
+				float x1 = (float) (x / 20.0);
+				float cellYSmall = row;
+				float cellYBig = row + 1;
+				float cellXSmall = col;
+				float cellXBig = col + 1;
+				
+				if ((priorX != 500.0)
+					&& ((priorX < cellXSmall && x1 > cellXSmall && (double) row <= y1 && (double) row >= y1 - 1.0)
+                        || (priorX > cellXBig && x1 < cellXBig && (double) row <= y1 && (double) row >= y1 - 1.0)
+                        || (priorY > cellYBig && y1 < cellYBig && (double) col <= x1 && (double) col >= x1 - 1.0)
+                        || (priorY < cellYSmall && y1 > cellXSmall && (double) col <= x1 && (double) col >= x1 - 1.0))
+					)
+				{
+                    
+					//make corresponding position in array alive or dead
+					if([[[grid objectAtIndex:row] objectAtIndex: col] integerValue] == 1)
+					{
+						[[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@0];
+                        //an abbreviation for [NSNumber numberWithInt: 0] is @0
+					}
+					else
+					{
+						[[grid objectAtIndex:row] replaceObjectAtIndex:col withObject:@1];
+                        //an abbreviation for [NSNumber numberWithInt: 1] is @1
+					}
+				}
+				priorX = x1;
+				priorY = y1;
+			}
+		}
+    }
+    
+}
+
+
 
 @end
